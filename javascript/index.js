48cc542f3bc7184dc3845ff3fd595cf2ad427cc3
Emmanuel Ayingre <123233002+Ayin223@users.noreply.github.com>
2023-03-01 19:14:18 +0000

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+const inputBtn = document.getElementById('input-btn')
+const inputEl = document.getElementById('input-el')
+
+let myLeads = []
+let myArray =[]
+
+
+let input= inputBtn.addEventListener('click', function (){
+    myArray.push('kfglfg;s')
+    console.log(myArray)
+})
+
+
+
